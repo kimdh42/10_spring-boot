ba13a5344e7c1fb48c1e6dbf45d2340354317339
kimdh42 <kimdh1992@gmail.com>
2024-04-02 09:36:23 +0900

    spring-boot

---
+++ chap05-exception-handler/src/main/java/com/ohgiraffers/exceptionhandler/ExceptionHandlerController.java
@@ -0,0 +1,45 @@
+package com.ohgiraffers.exceptionhandler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExceptionHandlerController {
+
+    @GetMapping("controller-null")
+    public String nullPointExceptionTest() {
+
+        String str = null;
+        System.out.println(str.charAt(0));
+
+        return "/";
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public String nullPointExceptionHandler(NullPointerException exception){
+
+        System.out.println("message : " + exception.getMessage());
+        System.out.println("controller(지역) 레벨의 exception 처리");
+
+        return "error/nullPointer";
+    }
+
+    @GetMapping("controller-user")
+    public String userExceptionTest() throws MemberRegistException {
+        boolean check = true;
+        if(check) {
+            throw new MemberRegistException("당신은 회원가입을 할 수 없어요!!");
+        }
+        return "/";
+    }
+
+    @ExceptionHandler(MemberRegistException.class)
+    public String userExceptionHandler(MemberRegistException exception, Model model) {
+
+        model.addAttribute("exception", exception);
+
+        return "error/memberRegist";
+    }
+}
